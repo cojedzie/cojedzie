5501e92149e6c7991509d9297bbb2226f7583ce8
Kacper Donat <kadet1090@gmail.com>
2018-09-19 22:44:21 +0200

    better js splits

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -6,41 +6,41 @@ import "leaflet/dist/leaflet.css";
 import Popper from 'popper.js';
 import * as $ from "jquery";
 
-import * as components from './components';
-
 window['$'] = window['jQuery'] = $;
 window['Popper'] = Popper;
 
 // dependencies
-import './font-awesome';
-import 'bootstrap'
 import { Vue } from "vue-property-decorator";
-
-import './filters'
 
 // async dependencies
 (async function () {
+    const [ components ] = await Promise.all([
+        import('./components'),
+        import('./font-awesome'),
+        import('./filters'),
+        import('bootstrap'),
+    ]);
+
+    // here goes "public" API
+    window['czydojade'] = {
+        components
+    };
+
+    window['app'] = new Vue({
+        el: '#app',
+        data: {
+            stops: [],
+            messages: {
+                count:   0,
+                visible: true
+            },
+            departures: {
+                state: ''
+            }
+        }, methods: {
+            handleMessagesUpdate(messages) {
+                this.messages.count = messages.length;
+            }
+        }
+    });
 })();
-
-// here goes "public" API
-window['czydojade'] = {
-    components
-};
-
-window['app'] = new Vue({
-    el: '#app',
-    data: {
-        stops: [],
-        messages: {
-            count:   0,
-            visible: true
-        },
-        departures: {
-            state: ''
-        }
-    }, methods: {
-        handleMessagesUpdate(messages) {
-            this.messages.count = messages.length;
-        }
-    }
-});
