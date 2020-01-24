306c034de562099131704db1783c5fd93a2698e5
Kacper Donat <kadet1090@gmail.com>
2020-01-24 22:26:29 +0100

    Add visual info about position of the stop on trip

--- resources/ts/components/trip.ts
+++ resources/ts/components/trip.ts
@@ -2,10 +2,24 @@ import Vue from "vue";
 import Component from "vue-class-component";
 import { Prop } from "vue-property-decorator";
 import { ScheduledStop } from "../model/trip";
+import { Stop } from "../model";
+
+type ScheduledStopInfo = ScheduledStop & { visited: boolean, current: boolean };
 
 @Component({ template: require("../../components/trip.html") })
 export class TripComponent extends Vue {
     @Prop(Array) public schedule: ScheduledStop[];
+    @Prop(Object) public current: Stop;
+
+    get stops(): ScheduledStopInfo[] {
+        let visited = true;
+
+        return this.schedule.map(stop => ({
+            ...stop,
+            current: stop.stop.id == this.current.id ? !(visited = false) : false,
+            visited: visited,
+        }));
+    }
 }
 
 Vue.component('Trip', TripComponent);
