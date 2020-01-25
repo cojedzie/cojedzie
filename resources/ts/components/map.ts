6df7bdbea01c9812598fe08c89b863c16ab57031
Kacper Donat <kadet1090@gmail.com>
2020-01-25 21:02:52 +0100

    Don't use OSM tiles

--- resources/ts/components/map.ts
+++ resources/ts/components/map.ts
@@ -2,6 +2,7 @@ import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
 import Vue from 'vue';
 
 import * as L from 'leaflet'
+import 'mapbox-gl-leaflet'
 import 'leaflet/dist/leaflet.css'
 
 import * as iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
@@ -12,8 +13,40 @@ delete L.Icon.Default.prototype['_getIconUrl'];
 
 L.Icon.Default.mergeOptions({ iconRetinaUrl, iconUrl, shadowUrl });
 
+export const LVectorLayer = Vue.extend({
+    props: {
+        url: {
+            type: String,
+            required: true,
+        },
+        token: {
+            type: String,
+            default: ""
+        },
+        attribution: String,
+    },
+    mounted(): void {
+        this['mapObject'] = L.mapboxGL({
+            style: this.url,
+            accessToken: this.token,
+            attribution: this.attribution
+        } as any);
+
+        this.$nextTick(() => {
+            const map = this.$parent['mapObject'];
+
+            this['mapObject'].addTo(map);
+        })
+    },
+    beforeDestroy(): void {
+        this.$parent['mapObject'].removeLayer(this['mapObject'])
+    },
+    render: () => null,
+});
+
 Vue.component('LMap', LMap);
 Vue.component('LTileLayer', LTileLayer);
+Vue.component('LVectorLayer', LVectorLayer);
 Vue.component('LMarker', LMarker);
 
 export { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
