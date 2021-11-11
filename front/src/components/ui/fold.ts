5c43f08c5dcf1a17770c75ddb58b1b5841c2dc55
Kacper Donat <kadet1090@gmail.com>
2021-11-11 17:55:01 +0100

    fix(front): Fix ui-fold edge cases and overflow
    Change inner workings of ui-fold to be more like state machine. It helps
    with edge cases when state of in-transition fold was changed. As a bonus
    it makes overflow of expanded fold visible so things like focused input
    outline are properly rendered.

--- front/src/components/ui/fold.ts
+++ front/src/components/ui/fold.ts
@@ -21,36 +21,99 @@ import { Options, Vue } from "vue-class-component";
 import { Prop, Watch } from "vue-property-decorator";
 import WithRender from "@templates/ui/fold.html"
 
+export type UiFoldState = "folded" | "expanding" | "expanded" | "folding";
+
 @WithRender
 @Options({ name: "UiFold" })
 export class UiFold extends Vue {
-    private observer: MutationObserver;
-
     @Prop(Boolean)
     public visible: boolean;
 
     @Prop(Boolean)
     public lazy: boolean;
 
-    mounted() {
-        this.resize();
+    public state: UiFoldState;
 
-        this.observer = new MutationObserver(() => this.resize());
-        this.observer.observe(this.$refs['inner'] as Node, {
-            characterData: true,
-            subtree: true,
-            childList: true
-        });
-    }
-
-    beforeDestroy() {
-        this.observer.disconnect();
+    data() {
+        return {
+            state: this.visible ? "expanded" : "folded"
+        }
     }
 
     @Watch('visible')
     private resize() {
+        if (this.visible) {
+            this.expand();
+        } else {
+            this.fold();
+        }
+    }
+
+    private handleTransitionEnd(event: TransitionEvent) {
+        if (event.target !== this.$el) {
+            // it's not about us
+            return;
+        }
+
+        const root = this.$el as HTMLDivElement;
+
+        switch (this.state) {
+            case "expanded":
+            case "expanding":
+                root.style.height = "auto";
+                this.state = "expanded";
+                break;
+            case "folded":
+            case "folding":
+                this.state = "folded";
+        }
+    }
+
+    private fold() {
+        const root = this.$el as HTMLDivElement;
+        const state = this.state;
+
+        switch (state) {
+            case "folded":
+            case "folding":
+                // no-op, it's already folded
+                break;
+
+            case "expanded":
+                root.style.height = `${ root.clientHeight }px`;
+
+                // DOM must be updated and style must be applied in order for this to work
+                setTimeout(() => {
+                    root.style.height = '0px';
+                    this.state = "folding";
+                })
+                break;
+
+            case "expanding":
+                // reverse process
+                root.style.height = '0px';
+                this.state = "folding";
+                break;
+        }
+    }
+
+    private expand() {
+        const root = this.$el as HTMLDivElement;
         const inner = this.$refs['inner'] as HTMLDivElement;
-        (this.$el as HTMLElement).style.height = `${ (this.visible && inner) ? inner.clientHeight : 0 }px`;
+        const state = this.state;
+
+        switch (state) {
+            case "expanded":
+            case "expanding":
+                // no-op, it's already folded
+                break;
+
+            case "folded":
+            case "folding":
+                root.style.height = inner.clientHeight + 'px';
+                this.state = "expanding";
+                break;
+        }
     }
 }
 
