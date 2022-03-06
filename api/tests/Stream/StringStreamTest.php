8f4c4540371a408a2121b80c120dbf0071565881
Kacper Donat <kadet1090@gmail.com>
2022-03-06 20:19:13 +0100

    test(api): Add tests for StringStream

---
+++ api/tests/Stream/StringStreamTest.php
@@ -0,0 +1,86 @@
+<?php
+/*
+ * Copyright (C) 2022 Kacper Donat
+ *
+ * @author Kacper Donat <kacper@kadet.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace App\Tests\Stream;
+
+use App\Parser\Exception\EndOfStreamException;
+use App\Parser\StringStream;
+use PHPUnit\Framework\TestCase;
+
+class StringStreamTest extends TestCase
+{
+    public function testStreamAllowsReading()
+    {
+        $stream = new StringStream('1234567890');
+
+        $this->assertEquals('123', $stream->read(3));
+        $this->assertEquals('456', $stream->read(3));
+    }
+
+    public function testStreamAllowsPeekingWithoutReading()
+    {
+        $stream = new StringStream('1234567890');
+
+        $this->assertEquals('123', $stream->peek(3));
+        $this->assertEquals('123', $stream->peek(3));
+        $this->assertEquals('123', $stream->read(3));
+    }
+
+    public function testStreamReportsEndOfStreamCorrectly()
+    {
+        $stream = new StringStream('123');
+
+        $this->assertEquals('123', $stream->read(3));
+        $this->assertTrue($stream->eof());
+    }
+
+    public function testStreamDoesNotAllowToReadAfterEof()
+    {
+        $stream = new StringStream('123');
+
+        $this->assertEquals('123', $stream->read(3));
+        $this->expectException(EndOfStreamException::class);
+
+        $stream->read(1);
+    }
+
+    public function testStreamDoesNotAllowToPeekAfterEof()
+    {
+        $stream = new StringStream('123');
+
+        $this->assertEquals('123', $stream->read(3));
+        $this->expectException(EndOfStreamException::class);
+
+        $stream->peek(1);
+    }
+
+    public function testStreamCanTellPosition()
+    {
+        $stream = new StringStream('1234567890');
+
+        $stream->read(3);
+        $position = $stream->tell();
+        $this->assertEquals(3, $position->offset);
+
+        $stream->read(3);
+        $position = $stream->tell();
+        $this->assertEquals(6, $position->offset);
+    }
+}
