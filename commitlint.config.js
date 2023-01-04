debaa6956c0c99fdc3b855c7b56ce9fc97cc6a4e
Kacper Donat <kadet1090@gmail.com>
2023-01-04 16:31:15 +0100

    chore: Add commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'body-case': [2, 'always', 'sentence-case'],
+        'subject-case': [2, 'always', 'sentence-case'],
+        'body-max-line-length': [1, 'always', 80],
+        'header-max-length': [1, 'always', 80],
+        'type-enum': [2, 'always', [
+            'build',
+            'chore',
+            'ci',
+            'docs',
+            'feat',
+            'fix',
+            'perf',
+            'refactor',
+            'revert',
+            'security',
+            'style',
+            'test',
+            'release'
+        ]],
+        'scope-enum': [2, 'always', ['front', 'api']],
+    },
+    ignores: [
+        commit => commit.startsWith('WIP ')
+    ]
+};
