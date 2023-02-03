f43d647835fb7bd128938a6c437387c5a2d77bbe
Kacper Donat <kadet1090@gmail.com>
2023-02-03 19:29:11 +0100

    perf(api): Add sentry support

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -15,4 +15,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
 ];
