bcdc727b8957a644e033b13b7fe8d01ca9dbffc3
Kacper Donat <kadet1090@gmail.com>
2023-06-04 22:33:38 +0200

    chore(api): MZKZG - Improve line extracting regex

--- api/src/Provider/ZtmGdansk/ZtmGdanskMessageLineExtractor.php
+++ api/src/Provider/ZtmGdansk/ZtmGdanskMessageLineExtractor.php
@@ -10,7 +10,7 @@ class ZtmGdanskMessageLineExtractor
 {
     public function extractLinesFromMessage(Message $message): Collection
     {
-        if (preg_match_all('/lini(?:i?|ach)\s*(?:nr\.?)?:?\s+((?:(?-i:[A-Z]{1,2}|\d+|[A-Z]{1,2}\d+)\b\s*(?:[,;]|i|oraz)?\s*)*)\.?/imu', $message->getMessage(), $matches) === 0) {
+        if (preg_match_all('/lini(?:i?|ach)\s*(?:nr\.?|autobusow(?:ych|ej)|tramwajow(?:ych|ej))?:?\s+((?:(?-i:[A-Z]{1,2}|\d+|[A-Z]{1,2}\d+)\b\s*(?:[,;]|i|oraz)?\s*)*)\.?/imu', $message->getMessage(), $matches) === 0) {
             return new Set();
         }
 
